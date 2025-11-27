d991b15ee0056d10db847942ed33f6d208c30013
Mukund Kumar <mukund@Mukunds-MacBook-Air.local>
2025-11-28 01:10:17 +0530

    Added basic get & post Rest Api's

--- src/main/java/org/example/introtospringboot/Todo.java
+++ src/main/java/org/example/introtospringboot/Todo.java
@@ -1,10 +1,23 @@
 package org.example.introtospringboot;
 
+import java.util.ArrayList;
+
 public class Todo {
 	private int id;
 	private boolean completed;
 	private String title;
 	private int userId;
+
+    Todo(){
+
+    }
+    Todo(int id, boolean completed, String title, int userId) {
+        this.id = id;
+        this.completed = completed;
+        this.title = title;
+        this.userId = userId;
+
+    }
 
 	public void setId(int id){
 		this.id = id;
--- src/main/java/org/example/introtospringboot/TodoController.java
+++ src/main/java/org/example/introtospringboot/TodoController.java
@@ -1,13 +1,36 @@
 package org.example.introtospringboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TodoController {
+    private static List<Todo> todo;
 
-    @GetMapping("/")
-    public String getHome(){
-        return "Hello World!";
+    public TodoController(){
+        todo = new ArrayList<>();
+        todo.add(new Todo(1,false,"Todo 1",1));
+        todo.add(new Todo(2,true,"Todo 2",2));
+
     }
+
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos() {
+        return todo;
+    }
+
+    @PostMapping("/todos")
+    public Todo CreateTodo(@RequestBody Todo newTodo){
+        todo.add(newTodo);
+        return newTodo;
+
+    }
+
+
 }
