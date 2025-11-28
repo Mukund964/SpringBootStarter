bf12eb2c6d5ae4a585f208c06439a278112f9d92
Mukund Kumar <mukund@Mukunds-MacBook-Air.local>
2025-11-28 21:18:02 +0530

    Added Patch & Delete methods

--- src/main/java/org/example/introtospringboot/TodoController.java
+++ src/main/java/org/example/introtospringboot/TodoController.java
@@ -1,35 +1,53 @@
 package org.example.introtospringboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TodoController {
-    private static List<Todo> todo;
+    private static List<Todo> todos;
 
     public TodoController(){
-        todo = new ArrayList<>();
-        todo.add(new Todo(1,false,"Todo 1",1));
-        todo.add(new Todo(2,true,"Todo 2",2));
+        todos = new ArrayList<>();
+        todos.add(new Todo(1,false,"Todo 1",1));
+        todos.add(new Todo(2,true,"Todo 2",2));
 
     }
 
 
     @GetMapping("/todos")
     public List<Todo> getTodos() {
-        return todo;
+        return todos;
     }
 
     @PostMapping("/todos")
     public Todo CreateTodo(@RequestBody Todo newTodo){
-        todo.add(newTodo);
+        todos.add(newTodo);
         return newTodo;
 
+    }
+
+    @PatchMapping("/todo/{id}")
+    public Todo updateTodoTitle(@PathVariable int id, @RequestBody String newTitle){
+        for(Todo todo : todos){
+            if(id == todo.getId()){
+                todo.setTitle(newTitle);
+                return todo;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/todo/{id}")
+    public void deleteTodo(@PathVariable int id){
+        for(Todo todo : todos){
+            if(todo.getId() == id){
+                todos.remove(todo);
+                break;
+            }
+        }
     }
 
 
