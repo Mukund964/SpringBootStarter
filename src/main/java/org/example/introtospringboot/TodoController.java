0692580e95226140846e105b31657abb350a38bb
Mukund Kumar <mukund@Mukunds-MacBook-Air.local>
2025-11-30 13:48:10 +0530

    replaced constructor dependency injection with autowired annotation

--- src/main/java/org/example/introtospringboot/TodoController.java
+++ src/main/java/org/example/introtospringboot/TodoController.java
@@ -13,11 +13,10 @@ import java.util.List;
 public class TodoController {
     private static List<Todo> todos;
 
-
+    @Autowired
     private TodoService todoService;
 
-    public TodoController(TodoService todoService) {
-        this.todoService = todoService;
+    public TodoController() {
         todos = new ArrayList<>();
         todos.add(new Todo(1,false,"Todo 1",1));
         todos.add(new Todo(2,true,"Todo 2",2));
